0c3f734f2bc301f90151c2357db64bf20ebe7ea5
Ash <95955389+aahna-ashina@users.noreply.github.com>
2022-05-13 18:30:53 +0800

    feat: dw-43/implement-nextjs-server
    Implement "Connect Wallet" functionality

--- server/pages/download-pass.js
+++ server/pages/download-pass.js
@@ -1,3 +1,35 @@
+import { Provider, createClient } from 'wagmi'
+
+const client = createClient()
+
+function App() {
+  return (
+    <Provider client={client}>
+      <Profile />
+    </Provider>
+  )
+}
+
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+
+function Profile() {
+  const { data } = useAccount()
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  })
+  const { disconnect } = useDisconnect()
+
+  if (data)
+    return (
+      <div>
+        Connected to {data.address}
+        <button onClick={() => disconnect()}>Disconnect</button>
+      </div>
+    )
+  return <button onClick={() => connect()}>Connect Wallet</button>
+}
+
 export default function DownloadPass() {
     console.log('/download-pass')
     return (
@@ -7,15 +39,13 @@ export default function DownloadPass() {
                     Download Pass
                 </h1>
 
-                <button>
-                    Connect wallet
-                </button>
+                <App />
                 
                 <p></p>
 
                 <a href="/api/downloadPass">
                     <button>
-                        Download pass
+                        Download Pass
                     </button>
                 </a>
             </main>
