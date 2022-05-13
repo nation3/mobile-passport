392353390c0c0a7ce0c1fda32b24e2358bab1508
Ash <95955389+aahna-ashina@users.noreply.github.com>
2022-05-13 16:03:11 +0800

    feat: dw-43/implement-nextjs-server
    Add API endpoint: `/api/pushUpdate`

---
+++ server/pages/api/pushUpdate.js
@@ -0,0 +1,8 @@
+// req = HTTP incoming message, res = HTTP server response
+export default function handler(req, res) {
+    console.log('/api/pushUpdate')
+    
+    // TODO
+
+    res.status(200).json({ text: '// TODO' });
+}
