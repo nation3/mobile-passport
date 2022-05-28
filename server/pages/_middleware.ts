fb322540b32170a96b62e37d1de631b950f3ad3a
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-05-28 17:00:43 +0800

    refactor: implement basic auth compatible with vercel (#2)
    Vercel debugging

--- server/pages/_middleware.ts
+++ server/pages/_middleware.ts
@@ -6,40 +6,42 @@ export function middleware(req: NextRequest, event: NextFetchEvent) {
     const pageName = req.page.name
     console.log('pageName:', pageName)
 
-    if (pageName && pageName.startsWith('/api/push')) {
-        const authorizationHeader = req.headers.get('authorization')
-        console.log('authorizationHeader:', authorizationHeader)
+    return new Response(`pageName: "${pageName}"`)
 
-        let wrongCredentials : boolean = false
-        if (authorizationHeader) {
-            // Get header value from "Basic <value>"
-            const headerValueBase64 = authorizationHeader.split(' ')[1]
+    // if (pageName && pageName.startsWith('/api/push')) {
+    //     const authorizationHeader = req.headers.get('authorization')
+    //     console.log('authorizationHeader:', authorizationHeader)
 
-            // Decode from Base64
-            const headerValue = Buffer.from(headerValueBase64, 'base64').toString()
+    //     let wrongCredentials : boolean = false
+    //     if (authorizationHeader) {
+    //         // Get header value from "Basic <value>"
+    //         const headerValueBase64 = authorizationHeader.split(' ')[1]
 
-            // Extract values from "<username>:<password>"
-            const [username, password] = headerValue.split(':')
+    //         // Decode from Base64
+    //         const headerValue = Buffer.from(headerValueBase64, 'base64').toString()
+
+    //         // Extract values from "<username>:<password>"
+    //         const [username, password] = headerValue.split(':')
             
-            // Get environment variables
-            const basicAuthUsername = String(process.env.BASIC_AUTH_USERNAME)
-            const basicAuthPassword = String(process.env.BASIC_AUTH_PASSWORD)
+    //         // Get environment variables
+    //         const basicAuthUsername = String(process.env.BASIC_AUTH_USERNAME)
+    //         const basicAuthPassword = String(process.env.BASIC_AUTH_PASSWORD)
 
-            // Compare credentials
-            if ((username !== basicAuthUsername) || (password !== basicAuthPassword)) {
-                wrongCredentials = true
-            }
-        }
-        console.log('wrongCredentials:', wrongCredentials)
+    //         // Compare credentials
+    //         if ((username !== basicAuthUsername) || (password !== basicAuthPassword)) {
+    //             wrongCredentials = true
+    //         }
+    //     }
+    //     console.log('wrongCredentials:', wrongCredentials)
 
-        if (!authorizationHeader || wrongCredentials) {
-            // Perform Basic Auth
-            return new Response('401 Unauthorized', {
-                status: 401,
-                headers: {
-                    'WWW-Authenticate': 'Basic realm="Secure Area"',
-                },
-            })
-        }
-    }
+    //     if (!authorizationHeader || wrongCredentials) {
+    //         // Perform Basic Auth
+    //         return new Response('401 Unauthorized', {
+    //             status: 401,
+    //             headers: {
+    //                 'WWW-Authenticate': 'Basic realm="Secure Area"',
+    //             },
+    //         })
+    //     }
+    // }
 }
