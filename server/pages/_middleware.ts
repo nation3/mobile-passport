02f221d7b13a1dfcfe8e6c87239af53ad2b45099
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-05-28 16:48:56 +0800

    refactor: implement basic auth compatible with vercel (#2)
    Add middleware authentication

--- server/pages/_middleware.ts
+++ server/pages/_middleware.ts
@@ -1,20 +1,45 @@
 import type { NextFetchEvent, NextRequest } from 'next/server'
 
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
+export function middleware(req: NextRequest, event: NextFetchEvent) {
     console.log('middleware')
 
-    console.log('req.page.name:', req.page.name)
+    const pageName = req.page.name
+    console.log('pageName:', pageName)
 
-    const basicAuth = req.headers.get('authorization')
-    console.log('basicAuth:', basicAuth)
-  
-    if (!basicAuth) {
-        return new Response(
-            '401 Unauthorized', {
-            status: 401,
-            headers: {
-                'WWW-Authenticate': 'Basic realm="Secure Area"',
-            },
-        })
+    if (pageName && pageName.startsWith('/api/push')) {
+        const authorizationHeader = req.headers.get('authorization')
+        console.log('authorizationHeader:', authorizationHeader)
+
+        let wrongCredentials : boolean = false
+        if (authorizationHeader) {
+            // Get header value from "Basic <value>"
+            const headerValueBase64 = authorizationHeader.split(' ')[1]
+
+            // Decode from Base64
+            const headerValue = Buffer.from(headerValueBase64, 'base64').toString()
+
+            // Extract values from "<username>:<password>"
+            const [username, password] = headerValue.split(':')
+            
+            // Get environment variables
+            const basicAuthUsername = String(process.env.BASIC_AUTH_USERNAME)
+            const basicAuthPassword = String(process.env.BASIC_AUTH_PASSWORD)
+
+            // Compare credentials
+            if ((username !== basicAuthUsername) || (password !== basicAuthPassword)) {
+                wrongCredentials = true
+            }
+        }
+        console.log('wrongCredentials:', wrongCredentials)
+
+        if (!authorizationHeader || wrongCredentials) {
+            // Perform Basic Auth
+            return new Response('401 Unauthorized', {
+                status: 401,
+                headers: {
+                    'WWW-Authenticate': 'Basic realm="Secure Area"',
+                },
+            })
+        }
     }
 }
--- server/pages/api/pushNotification.ts
+++ server/pages/api/pushNotification.ts
@@ -1,55 +1,38 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import basicAuthCheck from "../../utils/basicAuthCheck"
 import { Passes } from "../../utils/Passes"
-
-async function performBasicAuth(req: NextApiRequest, res: NextApiResponse) {
-    console.log('performBasicAuth')
-    await basicAuthCheck(req, res)
-    return res
-}
 
 // req = HTTP incoming message, res = HTTP server response
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     console.log('/api/pushNotification')
 
-    performBasicAuth(req, res)
-        .then(response => {
-            console.log('then')
-            console.log('response.statusCode', response.statusCode)
-            if (response.statusCode != 200) {
-                console.error(`Basic Auth failed: ${response.statusCode} ${response.statusMessage}`)
-                res.status(response.statusCode).json({ statusCode: response.statusCode, message: response.statusMessage })
-            } else {
-                // Push notification to all the passes
-                console.log('Pushing notification...')
+    // Push notification to all the passes
+    console.log('Pushing notification...')
 
-                const { title } = req.query
-                console.log(`title: "${title}"`)
-                if (!title || (String(title).trim().length == 0)) {
-                    res.status(400).json({ error: 'Missing/empty parameter: title' })
-                    return
-                }
+    const { title } = req.query
+    console.log(`title: "${title}"`)
+    if (!title || (String(title).trim().length == 0)) {
+        res.status(400).json({ error: 'Missing/empty parameter: title' })
+        return
+    }
 
-                const { content } = req.query
-                console.log(`content: "${content}"`)
-                if (!content || (String(content).trim().length == 0)) {
-                    res.status(400).json({ error: 'Missing/empty parameter: content' })
-                    return
-                }
+    const { content } = req.query
+    console.log(`content: "${content}"`)
+    if (!content || (String(content).trim().length == 0)) {
+        res.status(400).json({ error: 'Missing/empty parameter: content' })
+        return
+    }
 
-                // Remove leading/trailing whitespace
-                const trimmedTitle = String(title).trim()
-                const trimmedContent = String(content).trim()
+    // Remove leading/trailing whitespace
+    const trimmedTitle = String(title).trim()
+    const trimmedContent = String(content).trim()
 
-                // Push notification
-                const notificationSent: boolean = Passes.pushNotification(trimmedTitle, trimmedContent);
-                console.log('notificationSent:', notificationSent)
+    // Push notification
+    const notificationSent: boolean = Passes.pushNotification(trimmedTitle, trimmedContent);
+    console.log('notificationSent:', notificationSent)
 
-                res.status(response.statusCode).json({ 
-                    notificationSent: notificationSent,
-                    title: trimmedTitle,
-                    content: trimmedContent
-                })
-            }
-        })
+    res.status(200).json({ 
+        notificationSent: notificationSent,
+        title: trimmedTitle,
+        content: trimmedContent
+    })
 }
