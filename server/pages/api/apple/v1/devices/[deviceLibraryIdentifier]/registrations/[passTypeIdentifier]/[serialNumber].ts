8ccf818cc65f68a6e0475c6f416dc714ecaf6809
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-08-19 18:54:00 +0800

    feat: add a web service to update passes (#8)
    ## Dework Task
    
    https://app.dework.xyz/nation3/app-2?taskId=2769e47b-509b-4974-ba78-0453b4e541b3
    
    ## Related GitHub Issue
    
    https://github.com/nation3/mobile-passport/issues/8
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->

---
+++ server/pages/api/apple/v1/devices/[deviceLibraryIdentifier]/registrations/[passTypeIdentifier]/[serialNumber].ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+// req = HTTP incoming message, res = HTTP server response
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('[serialNumber].js')
+
+  // Expected URL format:  /api/apple/v1/devices/[deviceLibraryIdentifier]/registrations/[passTypeIdentifier]/[serialNumber]
+  console.log('req.url:', req.url)
+
+  // Expected request method:  POST
+  console.log('req.method:', req.method)
+
+  // Extract variables from the request URL
+  const { deviceLibraryIdentifier, passTypeIdentifier, serialNumber } = req.query
+  console.log('deviceLibraryIdentifier:', deviceLibraryIdentifier)
+  console.log('passTypeIdentifier:', passTypeIdentifier)
+  console.log('serialNumber:', serialNumber)
+
+  // Authenticate the request using a shared secret
+  // TODO
+
+  // Register the pass
+  // TODO
+
+  res.status(418).json({
+    statusCode: 418,
+    statusMessage: "I'm a teapot"
+  })
+}
