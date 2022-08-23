68401fd62525aca4d87acba308956ac0afbdaa11
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-08-23 10:51:59 +0800

    feat(apple): require request method (#8)

--- server/pages/api/apple/v1/devices/[deviceLibraryIdentifier]/registrations/[passTypeIdentifier]/[serialNumber].ts
+++ server/pages/api/apple/v1/devices/[deviceLibraryIdentifier]/registrations/[passTypeIdentifier]/[serialNumber].ts
@@ -4,11 +4,19 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log('[serialNumber].js')
 
-  // Expected URL format:  /api/apple/v1/devices/[deviceLibraryIdentifier]/registrations/[passTypeIdentifier]/[serialNumber]
+  // Expected URL format:
+  //   /api/apple/v1/devices/[deviceLibraryIdentifier]/registrations/[passTypeIdentifier]/[serialNumber]
+  //   /api/apple/v1/devices/b33e3a3dccb3030333e3333da33333a3/registrations/pass.org.passport.nation3/333
   console.log('req.url:', req.url)
 
   // Expected request method:  POST
   console.log('req.method:', req.method)
+  if (req.method != 'POST') {
+    res.status(400).json({
+      statusCode: 400,
+      statusMessage: "Invalid request method: " + req.method
+    })
+  }
 
   // Extract variables from the request query
   console.log('req.query:', req.query)
@@ -16,6 +24,10 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log('deviceLibraryIdentifier:', deviceLibraryIdentifier)
   console.log('passTypeIdentifier:', passTypeIdentifier)
   console.log('serialNumber:', serialNumber)
+
+  // Extract variables from the request header
+  console.log('req.headers:', req.headers)
+  // TODO
 
   // Authenticate the request using a shared secret
   // TODO
