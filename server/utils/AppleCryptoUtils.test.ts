a186a8a348585cf8097cb75adce2f0ecd1e72938
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-08-24 18:01:32 +0800

    test(jest): enable unit testing

---
+++ server/utils/AppleCryptoUtils.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from '@jest/globals'
+import { AppleCryptoUtils } from './AppleCryptoUtils'
+
+test('calculateSha1Hash', () => {
+  const filePath = '../template-versions/apple/1/icon.png'
+  const actual = AppleCryptoUtils.calculateSha1Hash(filePath)
+  expect(actual).toBe(123)
+})
