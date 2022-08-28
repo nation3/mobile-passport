5136d02b0080ee4c157bcc3b85d34f3d72c85a73
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-08-28 17:29:47 +0800

    test (api): error when wrong username/password (#48)

--- server/cypress/e2e/pushNotification.cy.ts
+++ server/cypress/e2e/pushNotification.cy.ts
@@ -6,6 +6,20 @@ describe('/api/pushNotification', () => {
       failOnStatusCode: false
     }).then((response) => {
       expect(response.status).to.eq(401)
+      expect(response.body).to.contain('Unauthorized')
+    })
+  }),
+
+  it('error when wrong username/password', () => {
+    cy.request({
+      url: '/api/pushNotification',
+      headers: {
+        authorization: 'Basic ' + Buffer.from('usr123:pwd123').toString('base64')
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(401)
+      expect(response.body).to.contain('Unauthorized')
     })
   })
 })
