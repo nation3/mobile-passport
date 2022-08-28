8e7831a87d00e97c7e86c0bd6dcd6537a208b8a4
TannerGabriel <gabrieltanner.code@gmail.com>
2022-08-28 08:48:53 +0200

    Use matcher for middleware
    Signed-off-by: TannerGabriel <gabrieltanner.code@gmail.com>

--- server/middleware.ts
+++ server/middleware.ts
@@ -1,51 +1,51 @@
 import type { NextFetchEvent, NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
-import { config } from './utils/Config'
+import { config as authConfig } from './utils/Config'
 
-export function middleware(req: NextRequest, event: NextFetchEvent) {
+export function middleware(req: NextRequest, _: NextFetchEvent) {
   console.log('middleware')
 
   const pathName = req.nextUrl.pathname
   console.log('pathName:', pathName)
 
-  // Perform Basic Auth on these paths:
-  //   /api/pushNotification
-  //   /api/pushUpdate
-  if (pathName.startsWith('/api/push')) {
-    const authorizationHeader = req.headers.get('authorization')
-    console.log('authorizationHeader:', authorizationHeader)
+  const authorizationHeader = req.headers.get('authorization')
+  console.log('authorizationHeader:', authorizationHeader)
 
-    let wrongCredentials: boolean = false
-    if (authorizationHeader) {
-      // Get header value from "Basic <value>"
-      const headerValueBase64 = authorizationHeader.split(' ')[1]
+  let wrongCredentials: boolean = false
+  if (authorizationHeader) {
+    // Get header value from "Basic <value>"
+    const headerValueBase64 = authorizationHeader.split(' ')[1]
 
-      // Decode from Base64
-      const headerValue = Buffer.from(headerValueBase64, 'base64').toString()
+    // Decode from Base64
+    const headerValue = Buffer.from(headerValueBase64, 'base64').toString()
 
-      // Extract values from "<username>:<password>"
-      const [username, password] = headerValue.split(':')
+    // Extract values from "<username>:<password>"
+    const [username, password] = headerValue.split(':')
 
-      // Get environment variables
-      const basicAuthUsername = String(process.env.BASIC_AUTH_USERNAME)
-      const basicAuthPassword = String(process.env.BASIC_AUTH_PASSWORD)
-      // TODO: enable usage of Config.ts in _middleware.ts
-      // const basicAuthUsername = config.basicAuthUsername
-      // const basicAuthPassword = config.basicAuthPassword
+    // Get environment variables
+    const basicAuthUsername = String(process.env.BASIC_AUTH_USERNAME)
+    const basicAuthPassword = String(process.env.BASIC_AUTH_PASSWORD)
+    // TODO: enable usage of Config.ts in middleware.ts
+    // const basicAuthUsername = authConfig.basicAuthUsername
+    // const basicAuthPassword = authConfig.basicAuthPassword
 
-      // Compare credentials
-      if (username !== basicAuthUsername || password !== basicAuthPassword) {
-        wrongCredentials = true
-      }
+    // Compare credentials
+    if (username !== basicAuthUsername || password !== basicAuthPassword) {
+      wrongCredentials = true
     }
-    console.log('wrongCredentials:', wrongCredentials)
+  }
+  console.log('wrongCredentials:', wrongCredentials)
 
-    if (!authorizationHeader || wrongCredentials) {
-      // Perform Basic Auth
-      req.nextUrl.pathname = `/api/unauthorized`;
-      return NextResponse.rewrite(req.nextUrl);
-    }
+  if (!authorizationHeader || wrongCredentials) {
+    // Perform Basic Auth
+    req.nextUrl.pathname = `/api/unauthorized`;
+    return NextResponse.rewrite(req.nextUrl);
   }
 }
 
-// TODO: Add matcher
+// Perform Basic Auth on these paths:
+  //   /api/pushNotification
+  //   /api/pushUpdate
+export const config = {
+  matcher: '/api/:push*',
+}
