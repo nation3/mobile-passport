f3061a65821d538e7d9768d860346aad8ec19618
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-08-28 17:33:47 +0800

    test (api): error when missing title  (#48)

--- server/cypress/e2e/pushNotification.cy.ts
+++ server/cypress/e2e/pushNotification.cy.ts
@@ -21,6 +21,19 @@ describe('/api/pushNotification', () => {
       expect(response.status).to.eq(401)
       expect(response.body).to.contain('Unauthorized')
     })
+  }),
+
+  it('error when missing title', () => {
+    cy.request({
+      url: '/api/pushNotification',
+      headers: {
+        authorization: 'Basic ' + Buffer.from('username:password').toString('base64')
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(400)
+      expect(JSON.stringify(response.body)).to.contain('Missing/empty parameter: title')
+    })
   })
 })
 
