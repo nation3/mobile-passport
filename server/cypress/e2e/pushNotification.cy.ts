62ba22000d18d30156aa3fdcf3f3eb05c8bc01f9
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-08-31 13:06:56 +0800

    test(api): improve basic auth tests (#48)

--- server/cypress/e2e/pushNotification.cy.ts
+++ server/cypress/e2e/pushNotification.cy.ts
@@ -10,11 +10,37 @@ describe('/api/pushNotification', () => {
     })
   }),
 
-  it('error when wrong username/password', () => {
+  it('error when wrong username and wrong password', () => {
     cy.request({
       url: '/api/pushNotification',
       headers: {
         authorization: 'Basic ' + Buffer.from('usr123:pwd123').toString('base64')
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(401)
+      expect(response.body).to.contain('Unauthorized')
+    })
+  }),
+
+  it('error when wrong username and correct password', () => {
+    cy.request({
+      url: '/api/pushNotification',
+      headers: {
+        authorization: 'Basic ' + Buffer.from('usr123:password').toString('base64')
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(401)
+      expect(response.body).to.contain('Unauthorized')
+    })
+  }),
+
+  it('error when correct username and wrong password', () => {
+    cy.request({
+      url: '/api/pushNotification',
+      headers: {
+        authorization: 'Basic ' + Buffer.from('username:pwd123').toString('base64')
       },
       failOnStatusCode: false
     }).then((response) => {
--- server/cypress/e2e/pushUpdate.cy.ts
+++ server/cypress/e2e/pushUpdate.cy.ts
@@ -10,7 +10,7 @@ describe('/api/pushUpdate', () => {
     })
   }),
 
-  it('error when wrong username/password', () => {
+  it('error when wrong username and wrong password', () => {
     cy.request({
       url: '/api/pushUpdate',
       headers: {
@@ -23,7 +23,33 @@ describe('/api/pushUpdate', () => {
     })
   }),
 
-  it('success when correct username/password', () => {
+  it('error when wrong username and correct password', () => {
+    cy.request({
+      url: '/api/pushUpdate',
+      headers: {
+        authorization: 'Basic ' + Buffer.from('usr123:password').toString('base64')
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(401)
+      expect(response.body).to.contain('Unauthorized')
+    })
+  }),
+
+  it('error when correct username and wrong password', () => {
+    cy.request({
+      url: '/api/pushUpdate',
+      headers: {
+        authorization: 'Basic ' + Buffer.from('username:pwd123').toString('base64')
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(401)
+      expect(response.body).to.contain('Unauthorized')
+    })
+  }),
+
+  it('success when correct username and correct password', () => {
     cy.request({
       url: '/api/pushUpdate',
       headers: {
