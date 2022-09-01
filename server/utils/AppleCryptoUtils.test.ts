902da45f155baee9362de9845ad7b0a034f7cd9a
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-09-01 14:12:16 +0800

    test: generateManifestObject (#61)

--- server/utils/AppleCryptoUtils.test.ts
+++ server/utils/AppleCryptoUtils.test.ts
@@ -3,7 +3,35 @@ import { describe, expect, test } from '@jest/globals'
 import { AppleCryptoUtils } from './AppleCryptoUtils'
 
 test('calculateSha1Hash', () => {
-  const filePath = './template-versions/apple/1/icon.png'
-  const actual = AppleCryptoUtils.calculateSha1Hash(filePath)
+  const filePath : string = './template-versions/apple/1/icon.png'
+  const actual : string = AppleCryptoUtils.calculateSha1Hash(filePath)
   expect(actual).toBe('a9ce8210a5a240f8e6dd272ac395b26b33b6c7c1')
+}),
+
+test('generateManifestObject v1', () => {
+  const templateVersionDir : string = './template-versions/apple/1'
+  const manifestObject : JSON = AppleCryptoUtils.generateManifestObject(templateVersionDir)
+  expect(JSON.stringify(manifestObject)).toContain('icon.png')
+  expect(JSON.stringify(manifestObject)).toContain('icon@2x.png')
+  expect(JSON.stringify(manifestObject)).toContain('logo.png')
+  expect(JSON.stringify(manifestObject)).toContain('logo@2x.png')
+  expect(JSON.stringify(manifestObject)).toContain('logo@3x.png')
+  expect(JSON.stringify(manifestObject)).toContain('pass.json')
+  expect(JSON.stringify(manifestObject)).toContain('strip.png')
+  expect(JSON.stringify(manifestObject)).toContain('strip@2x.png')
+  expect(JSON.stringify(manifestObject)).toContain('strip@3x.png')
+})
+
+test('generateManifestObject v2', () => {
+  const templateVersionDir : string = './template-versions/apple/2'
+  const manifestObject : JSON = AppleCryptoUtils.generateManifestObject(templateVersionDir)
+  expect(JSON.stringify(manifestObject)).toContain('icon.png')
+  expect(JSON.stringify(manifestObject)).toContain('icon@2x.png')
+  expect(JSON.stringify(manifestObject)).toContain('logo.png')
+  expect(JSON.stringify(manifestObject)).toContain('logo@2x.png')
+  expect(JSON.stringify(manifestObject)).toContain('logo@3x.png')
+  expect(JSON.stringify(manifestObject)).toContain('pass.json')
+  expect(JSON.stringify(manifestObject)).toContain('strip.png')
+  expect(JSON.stringify(manifestObject)).toContain('strip@2x.png')
+  expect(JSON.stringify(manifestObject)).toContain('strip@3x.png')
 })
