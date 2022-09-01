931b88ff05964712bf21c666e7ac80ab77f37f77
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-09-01 13:11:16 +0800

    test: calculateSha1Hash (#61)

--- server/utils/AppleCryptoUtils.test.ts
+++ server/utils/AppleCryptoUtils.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, test } from '@jest/globals'
 import { AppleCryptoUtils } from './AppleCryptoUtils'
 
 test('calculateSha1Hash', () => {
-  const filePath = '../template-versions/apple/1/icon.png'
+  const filePath = './template-versions/apple/1/icon.png'
   const actual = AppleCryptoUtils.calculateSha1Hash(filePath)
-  expect(actual).toBe(123)
+  expect(actual).toBe('a9ce8210a5a240f8e6dd272ac395b26b33b6c7c1')
 })
