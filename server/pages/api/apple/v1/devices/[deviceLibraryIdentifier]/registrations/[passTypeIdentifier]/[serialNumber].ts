5e170a791d5085cf172b0b1bf26c15fb7cd901a8
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-09-02 19:31:14 +0800

    refactor: add try-catch

--- server/pages/api/apple/v1/devices/[deviceLibraryIdentifier]/registrations/[passTypeIdentifier]/[serialNumber].ts
+++ server/pages/api/apple/v1/devices/[deviceLibraryIdentifier]/registrations/[passTypeIdentifier]/[serialNumber].ts
@@ -10,48 +10,55 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   //   /api/apple/v1/devices/b33e3a3dccb3030333e3333da33333a3/registrations/pass.org.passport.nation3/333
   console.log('req.url:', req.url)
 
-  // Expected request method:  POST
-  console.log('req.method:', req.method)
-  if (req.method != 'POST') {
-    // Throw Error
+  try {
+    // Expected request method:  POST
+    console.log('req.method:', req.method)
+    if (req.method != 'POST') {
+      console.error('Wrong request method: ' + req.method)
+      throw new Error('Wrong request method: ' + req.method)
+    }
+
+    // Extract variables from the request query
+    console.log('req.query:', req.query)
+    const { deviceLibraryIdentifier, passTypeIdentifier, serialNumber } = req.query
+    console.log('deviceLibraryIdentifier:', deviceLibraryIdentifier)
+    console.log('passTypeIdentifier:', passTypeIdentifier)
+    console.log('serialNumber:', serialNumber)
+
+    // Extract authentication token from the "authorization" header
+    // Expected format:
+    //   authorization: 'ApplePass 0x3fbeb3ae33af3fb33f3d33333303d333a333aff33f3133efbc3330333adb333a'
+    const authorizationHeader : any = req.headers.authorization
+    console.log('authorizationHeader:', authorizationHeader)
+    const authenticationToken : string = authorizationHeader?.split(' ')[1]
+    console.log('authenticationToken:', authenticationToken)
+
+    // Authenticate the request using a shared secret
     // TODO
-  }
 
-  // Extract variables from the request query
-  console.log('req.query:', req.query)
-  const { deviceLibraryIdentifier, passTypeIdentifier, serialNumber } = req.query
-  console.log('deviceLibraryIdentifier:', deviceLibraryIdentifier)
-  console.log('passTypeIdentifier:', passTypeIdentifier)
-  console.log('serialNumber:', serialNumber)
+    // Extract push token from the request body (application/json)
+    // Expected format:
+    //   {
+    //     pushToken: '333d0b3c3f3b3a330f3d0333333b33a3b0f33c33b333a333333ece3ab33333c3'
+    //   }
+    const pushToken : string = req.body.pushToken
+    console.log('pushToken:', pushToken)
 
-  // Extract authentication token from the "authorization" header
-  // Expected format:
-  //   authorization: 'ApplePass 0x3fbeb3ae33af3fb33f3d33333303d333a333aff33f3133efbc3330333adb333a'
-  const authorizationHeader : any = req.headers.authorization
-  console.log('authorizationHeader:', authorizationHeader)
-  const authenticationToken : string = authorizationHeader?.split(' ')[1]
-  console.log('authenticationToken:', authenticationToken)
-
-  // Authenticate the request using a shared secret
-  // TODO
-
-  // Extract push token from the request body (application/json)
-  // Expected format:
-  //   {
-  //     pushToken: '333d0b3c3f3b3a330f3d0333333b33a3b0f33c33b333a333333ece3ab33333c3'
-  //   }
-  const pushToken : string = req.body.pushToken
-  console.log('pushToken:', pushToken)
-
-  // Register the pass
-  supabase
-      .from('registrations')
-      .insert([{ device_library_identifier: deviceLibraryIdentifier, serial_number: serialNumber }])
-      .then((result: any) => {
-        console.log('result:', result)
-        res.status(result.status).json({
-          status: result.status,
-          statusText: result.statusText
+    // Register the pass
+    supabase
+        .from('registrations')
+        .insert([{ device_library_identifier: deviceLibraryIdentifier, serial_number: serialNumber }])
+        .then((result: any) => {
+          console.log('result:', result)
+          res.status(result.status).json({
+            status: result.status,
+            statusText: result.statusText
+          })
         })
-      })
+  } catch (err: any) {
+    console.error('[serialNumber].js err:\n', err)
+    res.status(400).json({
+      error: err.message
+    })
+  }
 }
