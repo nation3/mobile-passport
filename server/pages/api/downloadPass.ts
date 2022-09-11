42ef3cf95686635618566825ef56a4371c5371d6
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-09-11 12:16:15 +0800

    test: include webservice url for debugging (#6)

--- server/pages/api/downloadPass.ts
+++ server/pages/api/downloadPass.ts
@@ -135,7 +135,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
                               } else {
                                 const latestUpdateDate: Date = new Date(latest_updates_result.data['time'])
                                 const latestUpdateTitle: string = latest_updates_result.data['title']
-                                const latestUpdateContent: string = latest_updates_result.data['content']
+                                const latestUpdateContent: string = latest_updates_result.data['content'] + '\r\n\r\n' + config.appleWebServiceUrl 
 
                                 // Populate the pass template
                                 const filePath: string = Passes.generatePass(
