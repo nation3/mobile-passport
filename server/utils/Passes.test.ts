59c28e6a2b25c496053555532229c40c678648d5
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-10-24 18:23:57 +0800

    test(apple): add unit test for v4 of the template

--- server/utils/Passes.test.ts
+++ server/utils/Passes.test.ts
@@ -120,6 +120,52 @@ test('generatePass - Apple v3 - no ENS name', () => {
   expect(fs.existsSync(filePath)).toBe(true)
 })
 
+test('generatePass - Apple v4', () => {
+  const platform: Platform = Platform.Apple
+  const templateVersion: number = 4
+  const passportID: string = Math.floor(1 + (Math.random() * 840)).toString() // [1, 840]
+  const issueDateTimestamp: number = 1662541136 // September 7, 2022 8:58:56 AM
+  const address: string = '0x394b00B5De4E6f30292aCaC37f810Dd0672E211E'
+  const ensName: string = 'vitalik.eth'
+  const latestUpdateTitle: string = 'N3GOV-15: Set Nation3\’s North Star metrics'
+  const latestUpdateContent: string = 'A North Star metric is paramount since it aligns everyone in the nation towards a particular, measurable goal.'
+  const filePath: string = Passes.generatePass(
+    platform,
+    templateVersion,
+    passportID,
+    issueDateTimestamp,
+    address,
+    ensName,
+    latestUpdateTitle,
+    latestUpdateContent
+  )
+  expect(filePath).toContain('passport_0x394b00B5De4E6f30292aCaC37f810Dd0672E211E.pkpass')
+  expect(fs.existsSync(filePath)).toBe(true)
+})
+
+test('generatePass - Apple v4 - no ENS name', () => {
+  const platform: Platform = Platform.Apple
+  const templateVersion: number = 4
+  const passportID: string = Math.floor(1 + (Math.random() * 840)).toString() // [1, 840]
+  const issueDateTimestamp: number = 1662541136 // September 7, 2022 8:58:56 AM
+  const address: string = '0x394b00B5De4E6f30292aCaC37f810Dd0672E211E'
+  const ensName: string = ''
+  const latestUpdateTitle: string = 'N3GOV-15: Set Nation3\’s North Star metrics'
+  const latestUpdateContent: string = 'A North Star metric is paramount since it aligns everyone in the nation towards a particular, measurable goal.'
+  const filePath: string = Passes.generatePass(
+    platform,
+    templateVersion,
+    passportID,
+    issueDateTimestamp,
+    address,
+    ensName,
+    latestUpdateTitle,
+    latestUpdateContent
+  )
+  expect(filePath).toContain('passport_0x394b00B5De4E6f30292aCaC37f810Dd0672E211E.pkpass')
+  expect(fs.existsSync(filePath)).toBe(true)
+})
+
 test('notifyPassesAboutLastUpdate - Platform.Google', async () => {
   const platform: Platform = Platform.Google
   try {
